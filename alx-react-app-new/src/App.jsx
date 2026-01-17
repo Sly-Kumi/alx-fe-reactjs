aeb154132aaced25c2f0d2c90c0f9a7044902e6b
Sly <sylvesterb.kumi@gmail.com>
2026-01-17 21:33:06 +0000

    Add inline styling to components in alx-react-app-new

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,21 @@
+import UserProfile from './components/UserProfile';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+    <UserProfile
+  name="Alice"
+  age="25"
+  bio="Loves hiking and photography"
+/>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
