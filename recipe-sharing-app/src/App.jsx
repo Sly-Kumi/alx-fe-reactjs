d710eb91115c6a041aa732bfbcdc95880594f982
Sly <sylvesterb.kumi@gmail.com>
2026-01-24 23:31:46 +0000

    Add advanced search and filtering with Zustand

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <BrowserRouter>
       <div style={{ padding: '20px' }}>
         <h1>Recipe Sharing App</h1>
+        <SearchBar />
         <AddRecipeForm />
 
         <Routes>
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -2,15 +2,19 @@ import { Link } from 'react-router-dom';
 import { useRecipeStore } from './recipeStore';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore((state) => state.recipes);
+  const filteredRecipes = useRecipeStore(
+    (state) => state.filteredRecipes
+  );
 
   return (
     <div>
       <h2>Recipes</h2>
-      {recipes.map((recipe) => (
+      {filteredRecipes.map((recipe) => (
         <div key={recipe.id}>
           <h3>
-            <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+            <Link to={`/recipes/${recipe.id}`}>
+              {recipe.title}
+            </Link>
           </h3>
           <p>{recipe.description}</p>
         </div>
