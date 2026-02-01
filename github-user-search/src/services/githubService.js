67cac09fe314abbef9f2a8579ae655dab0359b23
Sly <sylvesterb.kumi@gmail.com>
2026-02-01 16:20:28 +0000

    Implement GitHub user search with API integration

---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const githubApi = axios.create({
+  baseURL: 'https://api.github.com',
+  headers: {
+    Authorization: `token ${import.meta.env.VITE_APP_GITHUB_API_KEY}`,
+  },
+});
+
+export const fetchUserData = async (username) => {
+  const response = await githubApi.get(`/users/${username}`);
+  return response.data;
+};
